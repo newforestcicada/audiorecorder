7418b6d1570df4c21f457a72d75da418194bff63
Alex Rogers <acr@ecs.soton.ac.uk>
2014-03-21 20:31:57 +0000

    Add settings.h.

---
+++ src/ios/Settings.h
@@ -0,0 +1,9 @@
+//
+//  Setting.h
+//  AudioRecorder Plugin
+//
+//  Created by acr on 23/12/2012.
+//  Copyright (c) 2012 University of Southampton. All rights reserved.
+//
+
+#define SAMPLES_PER_SECOND	44100
